8317d9fede332b3a20093755dbaf49a4d7311cf9
Azeroyear <eastazy@gmail.com>
2019-05-04 09:24:38 +0900

    answerd

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,12 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    remaining_drink = amount
+    begin
+      bonus_drink = remaining_drink / 3
+      stay_drink = remaining_drink % 3
+      amount += bonus_drink
+      remaining_drink = bonus_drink + stay_drink
+    end while (remaining_drink) >= 3
+    amount
   end
 end
